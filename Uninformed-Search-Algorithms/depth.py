cada5979243bb10e41f24a79e320dba974cd5af7
Daedleco <0241338@up.edu.mx>
2023-02-17 10:48:30 -0600

    Update depth.py

--- Uninformed-Search-Algorithms/depth.py
+++ Uninformed-Search-Algorithms/depth.py
@@ -1,8 +1,27 @@
-def run():
-    print("Running Depth First Search")
-    start = input("Start node: ")
-    goal = input("Goal node: ")
-    print("Start: " + start)
-    print("Goal: " + goal)
+import functions as f
 
-    print("Done!")
+def depth_first_search(graph, origin, destiny, path=None):
+    if path is None:
+        path = []
+
+    path.append(origin) 
+    
+    if origin == destiny:
+        return path 
+    
+    for neighbor in graph[origin]:
+        neighbor=neighbor[0]
+        if neighbor not in path:
+            new_path = depth_first_search(graph, neighbor, destiny, path.copy())  
+            if new_path is not None:
+                return new_path
+    return None
+
+def run(graph):
+    print("\n\nDepth First Search:\n---------------------")
+    print("Origin: ", end="")
+    origin = input()
+    print("Destiny: ", end="")
+    destiny = input()
+    path = depth_first_search(graph, origin, destiny)
+    f.show_path(path, origin, destiny)
