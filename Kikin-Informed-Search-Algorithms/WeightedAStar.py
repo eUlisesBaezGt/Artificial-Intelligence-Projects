fcef188c36f6277e414d7967180ab5ab82b7fe61
eUlisesBaezGt <89673617+eUlisesBaezGt@users.noreply.github.com>
2023-04-25 15:33:07 -0600

    Update WeightedAStar.py

--- Kikin-Informed-Search-Algorithms/WeightedAStar.py
+++ Kikin-Informed-Search-Algorithms/WeightedAStar.py
@@ -1,11 +1,10 @@
 from queue import PriorityQueue
 
-from KAGraph import KAGraph as KAg
 
-
-def WeightedAStarSearch(graph, heuristics, origin="Arad", destination="Bucharest", weight=1.3):
+def WeightedAStarSearch(graph, heuristics, origin, destination):
+    weight = float(input("Enter w value: "))
     frontier = PriorityQueue()
-    frontier.put(origin, 0)
+    frontier.put(origin, heuristics.get_weight(origin, destination))
     came_from = {}
     cost_so_far = {origin: 0}
 
@@ -16,10 +15,10 @@ def WeightedAStarSearch(graph, heuristics, origin="Arad", destination="Bucharest
             break
 
         for next_node in graph.get_neighbors(current):
-            new_cost = cost_so_far[current] + weight*(int(graph.get_weight(current, next_node)))
+            new_cost = cost_so_far[current] + weight * (int(graph.get_weight(current, next_node)))
             if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
                 cost_so_far[next_node] = new_cost
-                priority = new_cost + weight*(int(heuristics.get_weight(next_node, destination)))
+                priority = new_cost + weight * (int(heuristics.get_weight(next_node, destination)))
                 frontier.put(next_node, priority)
                 came_from[next_node] = current
 
@@ -30,29 +29,3 @@ def WeightedAStarSearch(graph, heuristics, origin="Arad", destination="Bucharest
     path.reverse()
     print(f"Cost: {cost_so_far[destination]}")
     return path
-
-
-def main():
-    graph = KAg.Graph()
-    with open("graph.txt") as file:
-        lines = file.readlines()
-
-    for i in range(1, len(lines)):
-        origin, destination, weight = lines[i].split()
-        graph.add_edge(origin, destination, weight)
-
-    heuristics = KAg.Graph()
-    with open("heuristics.txt") as file:
-        lines = file.readlines()
-
-    for i in range(1, len(lines)):
-        origin, destination, weight = lines[i].split()
-        heuristics.add_edge(origin, destination, weight)
-
-    path = WeightedAStarSearch(graph, heuristics)
-
-    print(f"Path: {path}")
-
-
-if __name__ == "__main__":
-    main()
