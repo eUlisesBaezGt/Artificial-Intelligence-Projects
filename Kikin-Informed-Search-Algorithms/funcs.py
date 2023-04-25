adde3454845bc6f47e5f6d810ab248ab21dd24e3
eUlisesBaezGt <89673617+eUlisesBaezGt@users.noreply.github.com>
2023-04-25 15:25:55 -0600

    Update funcs.py

--- Kikin-Informed-Search-Algorithms/funcs.py
+++ Kikin-Informed-Search-Algorithms/funcs.py
@@ -6,27 +6,5 @@ def show_path(path):
                 print(path[i])
             else:
                 print(path[i], end=" -> ")
-
-
-def nodes_list(graph):
-    nodes = []
-    for node in graph.nodes:
-        if node not in nodes:
-            nodes.append(node.upper())
-    return nodes
-
-
-def check_origin(graph, origin):
-    nodes = nodes_list(graph)
-    if origin.upper() not in nodes:
-        print("Origin not found.")
-        return False
-    return True
-
-
-def check_destiny(graph, destiny):
-    nodes = nodes_list(graph)
-    if destiny.upper() not in nodes:
-        print("Destiny not found.")
-        return False
-    return True
+    else:
+        print("PATH NOT FOUND")
