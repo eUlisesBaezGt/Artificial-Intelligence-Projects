aaf3f5310e647fcf2b63942707be05b944c857b7
eUlisesBaezGt <89673617+eUlisesBaezGt@users.noreply.github.com>
2023-04-25 15:25:31 -0600

    Update BranchBound.py

--- Kikin-Informed-Search-Algorithms/BranchBound.py
+++ Kikin-Informed-Search-Algorithms/BranchBound.py
@@ -1,8 +1,7 @@
 from queue import PriorityQueue
-from KAGraph import KAGraph as KAg
 
 
-def BranchAndBound(graph, origin="Arad", destination="Bucharest"):
+def BranchAndBound(graph, origin, destination):
     visited = set()
     frontier = PriorityQueue()
     # Tuple contains the current path cost, current node, and the path so far
@@ -10,30 +9,12 @@ def BranchAndBound(graph, origin="Arad", destination="Bucharest"):
     while not frontier.empty():
         cost, node, path = frontier.get()
         if node == destination:
-            return cost, path
+            print(f"Cost: {cost}")
+            return path
         visited.add(node)
         for neighbor, weight in graph.nodes[node].items():
             if neighbor not in visited:
                 new_path = path + [neighbor]
                 bound = cost + int(weight)
                 frontier.put((bound, neighbor, new_path))
-    return None, None
-
-
-def main():
-    graph = KAg.Graph()
-    with open("graph.txt") as file:
-        lines = file.readlines()
-
-    for i in range(1, len(lines)):
-        origin, destination, weight = lines[i].split()
-        graph.add_edge(origin, destination, weight)
-
-    cost, path = BranchAndBound(graph)
-
-    print(f"Cost: {cost}")
-    print(f"Path: {path}")
-
-
-if __name__ == "__main__":
-    main()
+    return None
