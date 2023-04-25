7e21d9e9c4f8bba6b3bd500d3111f8a5cfffcdd3
Daedleco <0241338@up.edu.mx>
2023-04-25 17:00:38 -0600

    Add files via upload

---
+++ Informed-Search-Loyal-Mau/stochastic_hill_climb.py
@@ -0,0 +1,22 @@
+import random
+def stochastic_hill_climb(graph, origin, destination, ht):
+    
+    current_path = [origin]
+    
+    while current_path[-1] != destination:
+        current_node = current_path[-1]
+        print(current_node)
+        successors = [(ht[next_node], next_node) for next_node, _ in graph.content[current_node] if next_node not in current_path and ht[next_node] < ht[current_node]]
+        print(successors)
+        if not successors:
+            return None
+        else:
+            (cost, next_node) = random.choice(successors)
+            print(cost, next_node)
+            print(ht[current_node])
+            if cost < ht[current_node]:
+                current_path.append(next_node)
+            else:
+                return current_path
+    
+    return current_path
